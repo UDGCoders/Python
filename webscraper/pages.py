4ed9eedc119e8e0f49a990ee28f9a1810fc466c6
UDGCoders <umarasif650@gmail.com>
2026-04-13 11:27:21 +0500

    extracting from html pages

--- webscraper/pages.py
+++ webscraper/pages.py
@@ -1,26 +1,28 @@
-import os 
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 import csv
 
-for page in range(1,6):
-    site_url=f"https://quotes.toscrape.com/page/{page}"
+with open('files/loop.csv', 'w', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Quote', 'Author'])
 
-    print(f"Connecting to {site_url}")
-    response=requests.get(site_url)
-    if response.status_code == 200:
-        print(f"Connected to site{site_url}")
-        
-    soup=BeautifulSoup(response.content,'html.parser')
+    for page in range(1, 6):
+        url = f"https://quotes.toscrape.com/page/{page}/"
+        print(f"Connecting to {url}")
 
-    qoutes=soup.find_all('div',class_='quote')
+        response = requests.get(url)
 
-    with open('files/looop.csv','w',newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(['Qoute','Author'])
-        for item in qoutes:
-            # print(item)
-            author=soup.find('small',class_='author').text.strip()
-            quote=soup.find('span',class_='text').text.strip()
-            writer.writerow([quote,author])
-        writer.writerow(['data wrriten'])
+        if response.status_code != 200:
+            print("Failed to connect")
+            continue
+
+        soup = BeautifulSoup(response.content, 'html.parser')
+        quotes = soup.find_all('div', class_='quote')
+
+        for quote in quotes:
+            text = quote.find('span', class_='text').text.strip()
+            author = quote.find('small', class_='author').text.strip()
+
+            writer.writerow([text, author])
+
+print("All data saved successfully")
