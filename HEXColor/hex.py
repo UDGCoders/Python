acd694500c6e2d3f4fe85d3ea5d40ec9e35d7015
UDGCoders <umarasif650@gmail.com>
2026-04-15 14:12:20 +0500

    finding text start with hash or not

--- HEXColor/hex.py
+++ HEXColor/hex.py
@@ -1,11 +1,19 @@
-charRange=['a','b','c','d','e','f']
-numbRange=[1,2,3,4,5,6]
+charRange=['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
 
-colorCode=input("Enter Color Code: ")
-if len(colorCode)<3 or len(colorCode) > 6:
-    print("color code is atleast 3 chars")
-else:
-    if '#' in colorCode:
+
+colorCode=input("Enter Color Code: ").lower().strip()
+colorscodes=[]
+if colorCode.startswith("#"):
+    cleanCode=colorCode[1:]
+    # print(colorCode)
+    if all(char in charRange for char in cleanCode) and len(cleanCode) in (3,6):
+        print('all values exists')
         print("Valid Starting") 
+        colorscodes.append(colorCode)
     else:
-        print("not a valid starting point")
+        print("not matched ")
+else:
+    print("not a valid starting point")
+
+print(colorscodes)
+
